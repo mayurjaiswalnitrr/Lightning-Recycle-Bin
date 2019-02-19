19e74e83ea4bdc527995fc8d68fbca76497b45d5
Mayur Jaiswal <47656049+mayurjaiswalnitrr@users.noreply.github.com>
2019-02-19 10:44:26 +0530

    Update LRC_CustomSearchHelper.js

--- LRC_CustomSearchHelper.js
+++ LRC_CustomSearchHelper.js
@@ -80,5 +80,60 @@
             });
 			$A.enqueueAction(action);            
         }
-	}
+	},
+    
+    sortColumn : function(component, event, field) {
+        console.log(field);
+        let sortAsc = component.get("v.sortAsc");
+        let sortField = field;
+        sortAsc = !sortAsc;
+        component.set("v.isLoading",true);
+        let object = component.get("v.userInputWrapper.sObjectName");
+        let action = component.get("c.getDeletedRecords");
+        action.setParams({
+            userInput : JSON.stringify(component.get("v.userInputWrapper")),
+            sortField : sortField,
+            isAsc : sortAsc
+        });
+        action.setCallback(this,function(response){
+            let state=response.getState();
+            component.set("v.isLoading",false);
+            if(state == "SUCCESS"){
+                let dRecords = JSON.parse(response.getReturnValue());
+                component.set("v.sortAsc", sortAsc);
+                component.set("v.sortField",sortField);
+                component.set("v.deletedRecords",JSON.parse(response.getReturnValue()));
+                let countSelected = parseInt(component.get("v.recordsCount"));
+                let start = parseInt(component.get("v.startIndex"));
+                let tempArray = dRecords.slice(start,(start  + countSelected));
+                component.set("v.tempRecords",tempArray);
+                if((dRecords.length - (start + countSelected) ) > 0){
+                    component.set("v.next",false);
+                }
+                else{
+                    component.set("v.next",true);
+                }
+                component.set("v.startIndex",start);
+                let end = start + tempArray.length;
+                if((start - 1) <= 0){
+                    component.set("v.previous",true);
+                }
+                else{
+                    component.set("v.previous",false);
+                }
+                start = start + 1;
+                let labelString = start.toString() + "-" + end.toString() + " of " + dRecords.length.toString();
+                component.set("v.selectLabel",labelString);
+                component.set("v.disable",false);
+            }
+            else{
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "message": 'Some error occured, please try again later!'
+                });
+                toastEvent.fire();
+            }
+        });
+        $A.enqueueAction(action);
+    } 
 })
